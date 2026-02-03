9c53b964d853f6f64e5dbcc563531f43a57c4077
Satish <satishdevops777@gmail.com>
2026-02-03 16:53:11 +0530

    auto-roboshop

---
+++ auto_shell_roboshop.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+COL="\e[32m"
+NC="\e[0m"
+
+echo -e "${COL}Auto Shell Roboshop${NC}"
+
+# $1 → component name (frontend, cart, user, etc.)
+# $2 → SSH password
+
+run_script() {
+  COMPONENT=$1
+  PASSWORD=$2
+  HOST="${COMPONENT}-dev.devopspro789.online"
+
+  echo "Running script on $HOST"
+
+  sshpass -p "$PASSWORD" scp ${COMPONENT}.sh centos@${HOST}:/tmp/
+
+  sshpass -p "$PASSWORD" ssh centos@${HOST} "
+    chmod +x /tmp/${COMPONENT}.sh &&
+    bash /tmp/${COMPONENT}.sh
+  "
+}
+
+run_script "$1" "$2"
