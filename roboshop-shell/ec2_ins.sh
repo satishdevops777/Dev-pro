7129a47276ba21b5e155efb905afa31c1dd34899
Satish <satishdevops777@gmail.com>
2026-02-08 22:02:17 +0530

    roboshop

--- roboshop-shell/ec2_ins.sh
+++ roboshop-shell/ec2_ins.sh
@@ -3,8 +3,7 @@
 ##### Change these values ###
 ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{ID:Id,Name:Name,Private:Config.PrivateZone}" --output text | awk '{print $1}' | awk -F / '{print $3}')
 DOMAIN_NAME=$(aws route53 list-hosted-zones --query "HostedZones[*].{ID:Id,Name:Name,Private:Config.PrivateZone}" --output text | awk '{print $2}' | sed -e 's/.$//')
-SG_NAME="allow-all"
-AWS_REGION="us-east-1"
+SG_NAME="roboshop-sg"
 #ENV="dev"
 #############################
 sudo rm -f /tmp/record.json
@@ -16,7 +15,7 @@ create_ec2() {
       --image-id ${AMI_ID} \
       --instance-type t3.small \
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"  \
-      --security-group-ids ${SGID} --region ${AWS_REGION} | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+      --security-group-ids ${SGID} | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
   sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" -e "s/DOMAIN_NAME/${DOMAIN_NAME}/" /tmp/labautomation/aws/route53.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
@@ -24,20 +23,20 @@ create_ec2() {
 
 
 ## Main Program
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" --region ${AWS_REGION} | jq '.Images[].ImageId' | sed -e 's/"//g')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 if [ -z "${AMI_ID}" ]; then
   echo "AMI_ID not found"
   exit 1
 fi
 
-SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} --region ${AWS_REGION} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 if [ -z "${SGID}" ]; then
   echo "Given Security Group does not exit"
   exit 1
 fi
 
 
-for component in catalogue-a cart-a user-a shipping-a payment-a frontend-a mongodb-a mysql-a rabbitmq-a redis-a dispatch-a; do
+for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
   COMPONENT="${component}"
   create_ec2
 done
