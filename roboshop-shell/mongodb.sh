d0fcb0cc0d78e18f5c56c2a2b60c9ddf538dace1
Satish <satishdevops777@gmail.com>
2026-02-11 13:40:29 +0530

    roboshop

--- roboshop-shell/mongodb.sh
+++ roboshop-shell/mongodb.sh
@@ -3,7 +3,6 @@ source common.sh
 mongodb_setup
 
 echo -e "${COL}Load update listen address${NC}"
-sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf &>>${LOG}
-
+sudo sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG}
 systemd_setup 
 
