ed654b3b550c50a53b72407abd8431132869ec1e
Satish <satishdevops777@gmail.com>
2026-02-11 21:08:09 +0530

    roboshop

--- roboshop-shell/shipping.sh
+++ roboshop-shell/shipping.sh
@@ -4,8 +4,10 @@ maven_setup
 
 echo -e "${COL}Install Mysql Client${NC}"
 dnf install mysql -y &>>${LOG}
+stat_check $?
 
 echo -e "${COL}Load Shipping Schema to Mysql${NC}"
 mysql --host=mysql-dev.devpro18.store -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>${LOG}
+stat_check $?
 
 systemd_setup
